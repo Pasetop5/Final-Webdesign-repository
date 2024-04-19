033b5139226056602945d8fae0f17e52f579af64
Pase Temitope <pasetope@gmail.com>
2024-04-19 16:57:16 +0100

    commit

--- Javascript/store.js
+++ Javascript/store.js
@@ -1,10 +1,7 @@
-
-/*Javascript for Product and Men's Page by Tope*/
-
+/* Javascript for Product and Men's Page by Tope */
 
 // for all elements with the class "more-info-btn" (all "Purchase" buttons)
 const purchaseButtons = document.querySelectorAll('.more-info-btn');
-
 
 // Function to handle purchase button click
 function purchaseHandler() {
@@ -14,4 +11,17 @@ function purchaseHandler() {
 // Loop through each purchase button and add event listener
 purchaseButtons.forEach(button => {
     button.addEventListener('click', purchaseHandler);
+});
+
+// for all elements with the class "product-img"
+const productImages = document.querySelectorAll('.product-img');
+
+// Function to handle product image click
+function productImageHandler() {
+    alert("Orders coming soon, please click on more info to fill the form and be on the waiting list");
+}
+
+// Loop through each product image and add event listener
+productImages.forEach(image => {
+    image.addEventListener('click', productImageHandler);
 });
